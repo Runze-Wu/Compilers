633bb6d0cbd2843009ae7dc09337dd47fb39c7f0
wurunzenull <181860109@smail.nju.edu.cn>
2021-03-04 23:15:56 +0800

    [update] 修改main正确调用lexical

--- lab1/Lab/Code/main.c
+++ lab1/Lab/Code/main.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 int curr_lineno=1;
-FILE *fin; //This is the file pointer from which the lexer reads its input.
-
+FILE *yyin; //This is the file pointer from which the lexer reads its input.
+extern int yylex();
 int main(int argc,char**argv){
     if(argc>1){
-        if(!(fin=fopen(argv[1],"r"))){
+        if(!(yyin=fopen(argv[1],"r"))){
             perror(argv[1]);
             return 1;
         }
+        printf("open file:%s\n",argv[1]);
         curr_lineno=1;
     }
-    while (yylex()!=0);
-    fclose(fin);
+    while(yylex()!=0);
+    fclose(yyin);
     return 0; 
 }
