50b8c6661feb13f2a406b0000a6159a25255fc59
wurunzenull <181860109@smail.nju.edu.cn>
2021-03-22 21:09:06 +0800

    [update] 增加debug模式

--- lab1/Lab/Code/main.c
+++ lab1/Lab/Code/main.c
@@ -14,21 +14,21 @@ int yyparse(void);                 // 语法分析的接口
 void yyrestart(FILE *input_file);  // 将yyin指针重置
 
 int main(int argc, char **argv) {
+    // yydebug = 1;
     if (argc == 1) return 1;
     if (argc > 1) {
         if (!(yyin = fopen(argv[1], "r"))) {
             perror(argv[1]);
             return 1;
         }
-        printf("open file:%s\n", argv[1]);
+        if (yydebug) printf("open file:%s\n", argv[1]);
     }
     yyrestart(yyin);
-    // yydebug = 1;
     yyparse();
     fclose(yyin);
     if (lexical_errs || syntax_errs) {
         /* meet erros */
-        printf("totally meet %d lexical errors and %d syntax errors\n", lexical_errs, syntax_errs);
+        if (yydebug) printf("totally meet %d lexical errors and %d syntax errors\n", lexical_errs, syntax_errs);
     } else {
         /* Print the AST tree */
         print_tree(root, 0);
