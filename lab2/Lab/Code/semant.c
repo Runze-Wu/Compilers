46a89eca0b094965ff5056b110acde6bcf5a46bd
wrznull <wrznull@gmail.com>
2021-04-26 17:02:33 +0800

    [update] Exp生成的ID必须为左值.

--- lab2/Lab/Code/semant.c
+++ lab2/Lab/Code/semant.c
@@ -320,7 +320,7 @@ Type Exp(Node root) {
     if (root->child_num == 1) {
         if (strcmp(get_child(root, 0)->name, "ID") == 0) {  // Exp -> ID
             result = look_up(get_child(root, 0)->val);
-            if (result == NULL || result->type->kind == STRUCTTAG) {
+            if (result == NULL || result->type->kind == STRUCTTAG || result->type->kind == FUNCTION) {
                 dump_semantic_error(1, root->line, "Undefined variable", get_child(root, 0)->val);
             } else {
                 type = result->type;
